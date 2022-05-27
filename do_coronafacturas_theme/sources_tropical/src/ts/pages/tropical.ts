9992643ef8424b04f400a54847bd595a1d137b6f
andresbonettm <88800530+andresbonettm@users.noreply.github.com>
2022-05-27 16:05:47 -0500

    validate firt load

--- do_coronafacturas_theme/sources_tropical/src/ts/pages/tropical.ts
+++ do_coronafacturas_theme/sources_tropical/src/ts/pages/tropical.ts
@@ -20,16 +20,22 @@ import SplitTextV2 from '../components/splitTextV2';
 
 export const page = function () {
     window.onload = function () {
-        initialize()
-        window.dispatchEvent(new Event('resize'));
-        window.scrollTo(0, 0)
-    }
-    window.addEventListener('resize', function () {
       initialize()
-    })
+      validateFirstLoad()
+      window.scrollTo(0, 0)
+    }
 }
 
 let tweens = []
+
+const validateFirstLoad = () => {
+  const pageLoad = localStorage.getItem('pageLoad');
+  if (!pageLoad) {
+    console.log("reset");
+    localStorage.setItem('pageLoad', '1');
+    location.reload()
+  }
+}
 
 function initialize() {
     const bannerInitial = new Swiper('.banner', {
